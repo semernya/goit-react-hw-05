62597f4f8f495def8f2cb650bc19b77cfebaf3c5
Anastasiia Semernya <semernya38@gmail.com>
2024-05-17 23:07:17 +0300

    --

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -8,12 +8,6 @@ const MoviesPage = lazy(() => import('../../pages/MoviesPage/MoviesPage'));
 const MovieDetailsPage = lazy(() => import('../../pages/MovieDetailsPage/MovieDetailsPage'));
 const MovieCast = lazy(() => import('../MovieCast/MovieCast'));
 const MovieReviews = lazy(() => import('../MovieReviews/MovieReviews'));
-// import HomePage from '../../pages/HomePage/HomePage';
-// import MoviesPage from '../../pages/MoviesPage/MoviesPage';
-// import MovieDetailsPage from '../../pages/MovieDetailsPage/MovieDetailsPage';
-// import MovieCast from '../MovieCast/MovieCast';
-// import MovieReviews from '../MovieReviews/MovieReviews';
-
 
 export default function App() {
 
